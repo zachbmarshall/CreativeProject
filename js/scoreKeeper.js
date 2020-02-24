576b4dd851092bffe1a8f7cd774f18752e2faedc
zachbmarshall <50930141+zachbmarshall@users.noreply.github.com>
2020-02-24 01:51:46 -0700

    add support for California Adventures Park

--- js/scoreKeeper.js
+++ js/scoreKeeper.js
@@ -19,14 +19,18 @@ class Player{
     }
 }
 
-let aoPlayers = [];
+var newPlayer = {
+    "name": "N/A",
+    "score": 0
+}
+
+var aoPlayers = [];
 
 function getNumPlayers(){
     document.getElementById("numPlayers").style.display = "none";
     document.getElementById("btnGetNumPlayers").style.display = "none";
     
     var iNumPlayers = parseInt(document.getElementById("numPlayers").value);
-    var oBtnStartGame = document.createElement("INPUT");
 
     document.getElementById("heading").innerHTML = "Enter the players names";
 
@@ -50,13 +54,17 @@ function getPlayerNames(){
 
     for (var iCount = 0; iCount < iNumPlayers; iCount++){
         sName = document.getElementById("player" + (iCount + 1) + "Score").value;
-        aoPlayers.push(new Player(sName));
+        newPlayer = {
+            "name": sName,
+            "score": 0
+        }
+        aoPlayers.push(newPlayer);
 
         var oInputField = document.getElementById("player" + (iCount+1) + "Score");
         oInputField.type = ("number");
 
-        document.getElementById("player" + (iCount + 1) + "Name").innerHTML = aoPlayers[iCount].name;
-        document.getElementById("player" + (iCount + 1) + "Total").innerHTML = aoPlayers[iCount].score;
+        document.getElementById("player" + (iCount + 1) + "Name").innerHTML = aoPlayers[iCount]["name"];
+        document.getElementById("player" + (iCount + 1) + "Total").innerHTML = aoPlayers[iCount]["score"];
     }
 
     document.getElementById("btnAddScore").style.display = "block";
@@ -78,7 +86,7 @@ function addScore(){
     if(bIsFull){
         for(var iCount = 0; iCount < aoPlayers.length; iCount++)
         {
-            aoPlayers[iCount].addScore(parseInt(document.getElementById("player" + (iCount + 1) + "Score").value));
+            aoPlayers[iCount]["score"] += parseInt(document.getElementById("player" + (iCount + 1) + "Score").value);
             document.getElementById("player" + (iCount + 1) + "Score").value = "";
         }
         displayScore();
@@ -95,7 +103,7 @@ function displayScore(){
     //bubble sorts the winners by score in descending order
     for (var iCount1 = 0; iCount1 < aoPlayers.length - 1; iCount1++){
         for (var iCount2 = iCount1 + 1; iCount2 < aoPlayers.length; iCount2++){
-            if (aoPlayers[iCount1].score < aoPlayers[iCount2].score){
+            if (aoPlayers[iCount1]["score"] < aoPlayers[iCount2]["score"]){
                 aoTempArray[iCount1] = aoPlayers[iCount1];
                 aoPlayers[iCount1] = aoPlayers[iCount2];
                 aoPlayers[iCount2] = aoTempArray[iCount1];
@@ -104,8 +112,8 @@ function displayScore(){
     }
 
     for(var iCount = 0; iCount < aoPlayers.length; iCount++){
-        document.getElementById("player" + (iCount + 1) + "Name").innerHTML = aoPlayers[iCount].name;
-        document.getElementById("player" + (iCount + 1) + "Total").innerHTML = aoPlayers[iCount].score;
+        document.getElementById("player" + (iCount + 1) + "Name").innerHTML = aoPlayers[iCount]["name"];
+        document.getElementById("player" + (iCount + 1) + "Total").innerHTML = aoPlayers[iCount]["score"];
 
     }
 }
